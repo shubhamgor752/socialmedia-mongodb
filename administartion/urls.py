7f309881a5d9f8068a52d046e881c296912acb3e
shubhamgor752 <shubhamgor752@gmail.com>
2024-07-18 18:48:51 +0530

    added in urls

--- administartion/urls.py
+++ administartion/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import SignInViewset
+from .views import SignInViewset , UserViewset , SwitchAccountViewSet
 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
 router.register("signin", SignInViewset, basename="signin")
+
+router.register("user/update", UserViewset, basename="user_update")
+
+router.register("switch/acc", SwitchAccountViewSet, basename="switch_account")
+
 
 urlpatterns = router.urls
