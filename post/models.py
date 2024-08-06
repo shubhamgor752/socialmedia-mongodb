3556a870c65700d3902826d15a92b7c11cf531de
shubhamgor752 <shubhamgor752@gmail.com>
2024-08-06 11:55:27 +0530

    create model post

--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from administartion.models import UserProfile, CustomUser
+
 
 # Create your models here.
+
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=65)
+    author = models.ForeignKey(UserProfile , on_delete = models.CASCADE , related_name = 'create_post')
+    likes = models.ManyToManyField(UserProfile , related_name='like_post', blank=True)
+    description = models.TextField()
